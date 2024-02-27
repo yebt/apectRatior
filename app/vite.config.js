7d46ac00dc0563c4622d68e79cc2924078c18048
Eduardo Bravo <yahir.eduardo.bravo@gmail.com>
2024-02-27 18:52:33 -0500

    feat(viteconfig): add some new aliases

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,11 +1,17 @@
-import { fileURLToPath, URL } from "node:url";
-import { defineConfig } from "vite";
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    resolve: {
-        alias: {
-            "@": fileURLToPath(new URL(',./src', import.meta.url))
-        }
-    }
+  base: './',
+  resolve: {
+    alias: [
+      { find: '@', replacement: resolve(__dirname, './src') },
+      { find: '@assets', replacement: resolve(__dirname, './src/assets') },
+      { find: '@styles', replacement: resolve(__dirname, './src/styles') },
+      { find: '@components', replacement: resolve(__dirname, './src/components') },
+      { find: '@modules', replacement: resolve(__dirname, './src/modules') },
+      { find: '@utils', replacement: resolve(__dirname, './src/utils') }
+    ]
+  }
 })
